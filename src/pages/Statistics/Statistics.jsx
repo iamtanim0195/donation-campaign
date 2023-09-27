5d7fb027148927626143cf6ab6c516241307a293
iamtanim0195 <sahadatmd195@gmail.com>
2023-09-27 23:37:38 +0600

    all done

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,23 +1,9 @@
-import { PieChart } from '@mui/x-charts/PieChart';
+import React from 'react'
 
 const Statistics = () => {
     return (
-        <div>
-            <PieChart
-                series={[
-                    {
-                        data: [
-                            { id: 0, value: 10, label: 'series A' },
-                            { id: 1, value: 15, label: 'series B' },
-                            { id: 2, value: 20, label: 'series C' },
-                        ],
-                    },
-                ]}
-                width={400}
-                height={200}
-            />
-        </div>
+        <div>Statistics</div>
     )
 }
 
-export default Statistics;
+export default Statistics
